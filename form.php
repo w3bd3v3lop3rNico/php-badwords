2286be87727f9e127777f0f241702c721f8535f8
Nicola Tabai <nicola.tabai98@gmail.com>
2023-11-21 18:14:19 +0100

    Chunck split on text

--- form.php
+++ form.php
@@ -2,6 +2,16 @@
 $text = $_POST['text'];
 
 $bad_word = $_POST['replace_word'];
+
+
+// chunck split
+$separator = '|';
+
+$length = 10;
+
+$chunk_split = chunk_split($text, $length, $separator);
+
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -15,6 +25,7 @@ $bad_word = $_POST['replace_word'];
     <p>Il testo è lungo <?php echo strlen($text); ?> caratteri</p>
     <p><?php echo str_replace($bad_word, '***', $text); ?></p>
     <p>Il nuovo testo è lungo <?php echo strlen(str_replace($bad_word, '***', $text));?> caratteri</p>
+    <p> <?php echo $chunk_split;?></p>
     
 </body>
 </html>
